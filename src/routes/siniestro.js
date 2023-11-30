07b0d6736a575a08ff82f03915fdfb33fa8b4dde
unknown <ivanjukonis@hotmail.com>
2023-11-30 12:28:23 -0300

    SA-BE: Added PUT and DELETE methods to the SINIESTRO entity

--- src/routes/siniestro.js
+++ src/routes/siniestro.js
@@ -7,6 +7,8 @@ const siniestroController = require('../controllers/siniestro');
 router
   .get('/', siniestroController.getSiniestro)
   .get('/:id', siniestroController.getSiniestroById)
-  .post('/', siniestroController.createSiniestro);
+  .post('/', siniestroController.createSiniestro)
+  .put('/:id', siniestroController.updateSiniestro)
+  .delete('/:id', siniestroController.deleteSiniestro);
 
 module.exports = router;
