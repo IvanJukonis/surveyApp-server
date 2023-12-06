050bc822f14f219d386f7cca4965f325d9a679b3
unknown <ivanjukonis@hotmail.com>
2023-12-06 14:27:04 -0300

    SA-BE: Implementation of model corrections into CONTROLADOR and RELEVADOR components

--- src/models/Controlador.js
+++ src/models/Controlador.js
@@ -5,87 +5,133 @@ const { Schema } = mongoose;
 const controladorSchema = new Schema({
   tipo: {
     type: String,
-    enum: [''],
+    enum: ['Relevador', 'Controlador', 'Administrativo', 'Consultor'],
     required: true,
   },
   nombre: {
-
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    required: true,
   },
   apellido: {
-
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    required: true,
   },
   email: {
-
+    type: String,
+    required: true,
+    match: /^[^@]+@[^@]+.[a-zA-Z]{2,}$/,
+    unique: true,
   },
   dni: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 8,
+    required: true,
   },
   fechaNacimiento: {
-
+    type: Date,
+    required: true,
   },
   fechaContratacion: {
-
+    type: Date,
+    required: true,
   },
   direccion: {
-
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 15,
   },
   localidad: {
-
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 15,
   },
   telefono: {
-
+    type: String,
+    required: true,
+    minLength: 9,
+    maxLength: 12,
   },
   contrato: {
     type: String,
-    enum: [''],
+    enum: ['Termino Fijo', 'Termino Indefinido', 'Termino Temporal', 'Labor'],
     required: true,
   },
   hsLaborales: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 8,
+    required: true,
   },
   salario: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 8,
+    required: true,
   },
   fechaActualizacionSalario: {
-
+    type: Date,
+    required: true,
   },
   numeroSeguridadSocial: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 8,
+    required: true,
   },
   oficina: {
     type: String,
-    enum: [''],
+    enum: ['Rosario', 'Vgg', 'San Lorenzo'],
     required: true,
   },
   departamento: {
     type: String,
-    enum: [''],
+    enum: ['Administracion', 'Produccion', 'Marketing', 'RRHH'],
     required: true,
   },
   puesto: {
     type: String,
-    enum: [''],
+    enum: ['Gerente', 'Empleado'],
     required: true,
   },
   cantidadHijos: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 8,
+    required: true,
   },
   estadoCivil: {
     type: String,
-    enum: [''],
+    enum: ['Casado/a', 'Soltero/a', 'Viudo/a', 'Divorciado/a'],
     required: true,
   },
   activo: {
-
+    type: Boolean,
+    default: true,
   },
   cuentaBancaria: {
-
+    type: Number,
+    minLength: 3,
+    maxLength: 15,
+    required: true,
   },
   nombreUsuario: {
-
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    required: true,
   },
   contraseña: {
-
+    type: String,
+    minLength: 3,
+    maxLength: 30,
+    required: true,
   },
 });
 
