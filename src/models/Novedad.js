9574cb1e50f7299c457d0427769cf37bde6bcee3
unknown <ivanjukonis@hotmail.com>
2023-12-10 19:43:59 -0300

    SA-FE: Properties added into model from NOVEDAD component

--- src/models/Novedad.js
+++ src/models/Novedad.js
@@ -3,36 +3,43 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const novedadSchema = new Schema({
+  siniestro: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Siniestro',
+    required: true,
+  },
   fecha: {
-
+    type: Date,
+    required: true,
   },
   hora: {
-
+    type: Date,
+    required: true,
   },
   titulo: {
-
+    type: String,
+    required: true,
   },
   tipo: {
     type: String,
-    enum: [''],
+    enum: ['Consulta', 'Notificacion', 'Aviso', 'Respuesta'],
     required: true,
   },
   relacion: {
     type: String,
-    enum: [''],
+    enum: ['CVA', 'LUGAR', 'CVT', 'PVT', 'PVA', 'TVT', 'TVA', 'VA', 'VT'],
     required: true,
   },
   descripcion: {
-
+    type: String,
+    required: true,
   },
   visibilidad: {
-    type: String,
-    enum: [''],
-    required: true,
+    type: Boolean,
+    default: true,
   },
   respuesta: {
     type: String,
-    enum: [''],
     required: true,
   },
 });
