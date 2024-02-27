cff4d5aa80fa67b88457c6ef7d1d3fd66404ed5b
unknown <ivanjukonis@hotmail.com>
2024-02-27 06:26:25 -0300

    SA-BE: Final presentation commit

--- src/routes/controlador.js
+++ src/routes/controlador.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const verifyToken = require('../middlewares/authMiddleware');
 
 const controladorController = require('../controllers/controlador');
 
 router
-  .get('/', controladorController.getControlador)
-  .get('/:id', controladorController.getControladorById)
+  .get('/', verifyToken, controladorController.getControlador)
+  .get('/:id', verifyToken, controladorController.getControladorById)
   .post('/', controladorController.postControlador)
   .put('/:id', controladorController.updateControlador)
   .delete('/:id', controladorController.deleteControlador);
